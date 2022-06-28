2f01dbb6cac4e44c983266d0ac91267de0e90f19
Arif6474 <arifulislam64743@gmail.com>
2022-06-28 12:17:49 +0600

    added reducer

--- src/components/redux.js
+++ src/components/redux.js
@@ -1,13 +1,10 @@
 // defining constant
 const INCREMENT = "INCREMENT"
 const DECREMENT = "DECREMENT"
-const ADD_USER = "ADD_USER"
+
 // state 
 const initialCounterState = {
-    count : 0,
-};
-const initialUsersState = {
-    user: [{name: "Arif islam"}]
+    count : 0
 };
 
 // action - object - type  , payload
@@ -22,13 +19,32 @@ const decrementCounter = () => {
         type : DECREMENT,
     };
 };
-// const addUser=() => {
-//     return {
-//         type : ADD_USER,
-//     }
-// }
 
 // 1. state 
 // 2. dispatch
 // 3. reducer
 // 4. store
+
+// create reducer for counter
+const reducerCounter = (state = initialCounterState, action)=>{
+    switch (action.type) {
+        case INCREMENT:
+            
+           return{
+               ...state,
+               count: state.count + 1
+           }
+        case DECREMENT:
+            
+           return{
+            ...state,
+               count: state.count - 1
+           }
+    
+        default:
+            // eslint-disable-next-line no-unused-expressions
+            state;
+    };
+}
+
+  
