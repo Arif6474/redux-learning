f1ea1719ddfc6438267e0c4c6308c1cc51a30af8
Arif6474 <arifulislam64743@gmail.com>
2022-12-01 12:43:32 +0600

    design parents and child components

--- src/components/Child.js
+++ src/components/Child.js
@@ -3,8 +3,11 @@ import React from 'react'
 export default function Child({count , setCount}) {
    
   return (
+    <div style={{ background : "gray" , width : "200px" , padding : "10px" , border : "1px solid yellow" , margin : "auto"}}>
     <div>
-        <h2>{count}</h2>
+        <p>Child</p>
+    <h2>{count}</h2>
+    </div>
         <button onClick={() =>setCount(count -1)}>DECREMENT</button>
         <button onClick={() =>setCount((prvState) => prvState + 1)}>INCREMENT</button>
     </div>
--- src/components/Parents.js
+++ src/components/Parents.js
@@ -1,14 +1,25 @@
-import React, { useState } from 'react';
-import Child from './Child';
+import React, { useState } from "react";
+import Child from "./Child";
 
 const Parents = () => {
-    const [count, setCount] = useState(0)
-    return (
-        <div>
-            <h2>{count}</h2>
-            <Child count={count} setCount={setCount}/>
-        </div>
-    );
+  const [count, setCount] = useState(0);
+  return (
+    <div
+      
+    >
+      <div style={{
+        background: "gray",
+        width: "200px",
+        padding: "10px",
+        border: "1px solid yellow",
+        margin: "20px auto",
+      }}>
+        <p>Parents</p>
+        <h2>{count}</h2>
+      </div>
+      <Child count={count} setCount={setCount} />
+    </div>
+  );
 };
 
 export default Parents;
